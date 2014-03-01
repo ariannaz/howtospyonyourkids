e984ce25db6dd210407253a836216ff35c54ef18
Victor Zhang <stetorvs@gmail.com>
2014-03-01 18:02:26 -0500

    Fix some minor compile errors and user strings.

--- TestApp/src/com/example/testapp/DisplayMessageActivity.java
+++ TestApp/src/com/example/testapp/DisplayMessageActivity.java
@@ -53,8 +53,7 @@ public class DisplayMessageActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.display_message, menu);
+		// Do nothing
 		return true;
 	}
 
