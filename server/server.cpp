319e75d9c8858907f93adc82a72713ad500e1b6a
Victor Zhang <stetorvs@gmail.com>
2014-03-02 22:18:13 -0500

    Do some bookkeeping on the server side and use C++11 in the Makefile and server.cpp

--- server/server.cpp
+++ server/server.cpp
@@ -8,9 +8,42 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <assert.h>
+
+#include <algorithm>
+#include <vector>
+#include <sstream>
+#include <unordered_map>
 
 #define PORT    3490
 #define MAXMSG  512
+
+using namespace std;
+
+typedef struct {
+  unsigned int id;
+  unsigned long long hash;
+  string name;
+  string ip;
+  unsigned short port;
+} ClientDevice;
+
+unsigned int get_client_id(const char *buffer, unordered_map<unsigned long long, unsigned int> &client_hash_to_id)
+{
+  unsigned long long hash;
+  sscanf(buffer, "%*s (%llx) connected", &hash);
+  auto find_it = client_hash_to_id.find(hash);
+  if (find_it == end(client_hash_to_id))
+    {
+      auto client_id = client_hash_to_id.size();
+      client_hash_to_id[hash] = client_id;
+      return client_id;
+    }
+  else
+    {
+       return find_it->second;
+    }
+}
 
 int make_socket (uint16_t port)
 {
@@ -38,16 +71,16 @@ int make_socket (uint16_t port)
   return sock;
 }
 
-int reply_to_client (int filedes)
+int reply_to_client (int filedes, unsigned int client_id)
 {
-  static const char confirm_msg[] = "Client confirmed\n";
-  int msg_size = strlen(confirm_msg) + 1;
-  int nbytes = write(filedes, confirm_msg, msg_size);
-  fprintf (stderr, "Server sent message: %swith nybtes successful: %d\n", confirm_msg, nbytes);
+  string confirm_msg = "Client #" + to_string(client_id) + " confirmed\n";
+  int msg_size = confirm_msg.length() + 1;
+  int nbytes = write(filedes, confirm_msg.c_str(), msg_size);
+  fprintf (stderr, "Server sent message: %swith nybtes successful: %d\n", confirm_msg.c_str(), nbytes);
   return nbytes >= 0;
 }
 
-int read_from_client (int filedes)
+int read_from_client (int filedes, const struct sockaddr_in clientname, unordered_map<unsigned long long, unsigned int> &client_hash_to_id, vector<ClientDevice> &client_devices)
 {
   char buffer[MAXMSG];
   int nbytes;
@@ -67,7 +100,21 @@ int read_from_client (int filedes)
       /* Data read. */
       buffer[nbytes] = '\0';
       fprintf (stderr, "Server: got message: '%s'\n", buffer);
-      return reply_to_client(filedes);
+      unsigned int client_id = get_client_id(buffer, client_hash_to_id);
+      if (client_id >= client_devices.size())
+        {
+          assert(client_id == client_devices.size());
+          string client_name;
+          stringstream ss;
+          ss.str(string(buffer));
+          ss >> client_name;
+          unsigned long long client_hash;
+          sscanf(buffer, "%*s (%llx) connected", &client_hash);
+          ClientDevice client_device = {client_id, client_hash, client_name, string(inet_ntoa(clientname.sin_addr)), ntohs(clientname.sin_port)};
+          client_devices.push_back(client_device);
+          fprintf(stderr, "Created Client Device %u %llx %s %s %hu\n", client_device.id, client_device.hash, client_device.name.c_str(), client_device.ip.c_str(), client_device.port);
+        }
+      return reply_to_client(filedes, client_id);
     }
 }
 
@@ -78,6 +125,11 @@ int main (void)
   int i;
   struct sockaddr_in clientname;
   socklen_t size;
+
+  // Keeping track of clients
+  unordered_map<unsigned long long, unsigned int> client_hash_to_id;
+  vector<ClientDevice> client_devices;
+  unordered_map<int, struct sockaddr_in> fd_to_sockaddr;
 
   /* Create the socket and set it up to accept connections. */
   sock = make_socket (PORT);
@@ -108,26 +160,28 @@ int main (void)
             if (i == sock)
               {
                 /* Connection request on original socket. */
-                int new_req;
+                int new_fd;
                 size = sizeof (clientname);
-                new_req = accept (sock,
+                new_fd = accept (sock,
                               (struct sockaddr *) &clientname,
                               &size);
-                if (new_req < 0)
+                if (new_fd < 0)
                   {
                     perror ("accept");
                     exit (EXIT_FAILURE);
                   }
                 fprintf (stderr,
-                         "Server: connect from host %s, port %hd.\n",
+                         "Server: connect from host %s, port %hu, fd %d.\n",
                          inet_ntoa (clientname.sin_addr),
-                         ntohs (clientname.sin_port));
-                FD_SET (new_req, &active_fd_set);
+                         ntohs (clientname.sin_port),
+                         new_fd);
+                fd_to_sockaddr[new_fd] = clientname;
+                FD_SET (new_fd, &active_fd_set);
               }
             else
               {
                 /* Data arriving on an already-connected socket. */
-                if (read_from_client (i) < 0)
+                if (read_from_client (i, fd_to_sockaddr.at(i), client_hash_to_id, client_devices) < 0)
                   {
                     close (i);
                     FD_CLR (i, &active_fd_set);
