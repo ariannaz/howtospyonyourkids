c9905a91594f88065b7d65883f0715f9577c8bad
Victor Zhang <stetorvs@gmail.com>
2014-03-24 21:17:47 -0400

    Add c-struct-like class representing a Client Device.

---
+++ TestApp/src/com/example/testapp/ClientDevice.java
@@ -0,0 +1,12 @@
+package com.example.testapp;
+
+import java.net.InetAddress;
+
+/** All public data members, similar to a C-struct */
+public class ClientDevice {
+	public int id;
+	public long hash;
+	public String name;
+	public InetAddress ip;
+	public int port;
+}
